f0ed2134db695d0dec2642015b69b338cec23b7c
cheuknam455 <icqmboy1106@gmail.com>
2026-04-19 19:13:21 +0800

    dd

---
+++ Assets/FastMesh/Scripts/SceneViewText.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+// We move this inside the check so the build doesn't fail
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace FastMesh_Example
+{
+    [ExecuteInEditMode]
+    public class SceneViewText : MonoBehaviour
+    {
+        public bool isShow = true;
+        string text2 = "These 3D models, all created with \"Fast Mesh - 3D Asset Creation Tool\" (click)"; 
+        Color backgroundColor = Color.white;
+        Color textColor = Color.black; 
+    
+        private void OnEnable()
+        {
+            #if UNITY_EDITOR
+            SceneView.duringSceneGui += OnSceneGUI;
+            #endif
+        }
+    
+        private void OnDisable()
+        {
+            #if UNITY_EDITOR
+            SceneView.duringSceneGui -= OnSceneGUI;
+            #endif
+        }
+
+        #if UNITY_EDITOR
+        private void OnSceneGUI(SceneView sceneView)
+        {
+            if (isShow == false) return;
+            
+            Handles.BeginGUI();
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = 20;
+            style.normal.textColor = textColor;
+            style.wordWrap = true;
+    
+            float width = 420f;
+            float height = 50f;
+            float x = (sceneView.position.width - width) / 2f;
+            float y = 10f;
+    
+            GUI.color = backgroundColor;
+            GUI.DrawTexture(new Rect(x - 10, y - 10, width + 20, height + 20), Texture2D.whiteTexture);
+            GUI.color = Color.white;
+            
+            if (GUI.Button(new Rect(x, y, width, height), text2, style))
+            {
+                Application.OpenURL("https://assetstore.unity.com/packages/slug/288711");
+            }
+            Handles.EndGUI();
+        }
+        #endif
+    }
+}
+
